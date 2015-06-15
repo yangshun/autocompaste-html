1974a7ba533cdef4560df927706ca2664705e89c
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-06-15 21:16:06 +0800

    Generate pre and post questionnaires on client side

--- js/ACPToolKit.js
+++ js/ACPToolKit.js
@@ -1,7 +1,6 @@
 var ACPToolKit = (function () {
-    // Depends on DataStorage. Must be loaded after it.
+    // ACPToolKit depends on DataStorage. Must be loaded after DataStorage.js.
     var module = {};
-    
 
     module.setCurrentParticipantId = function (pid) {
         DataStorage.setItem('pid', pid);
@@ -24,6 +23,40 @@ var ACPToolKit = (function () {
         });        
     }
 
+    module.generateFormInCSV = function (formResponses, type) {
+        var headers = [];
+        var data = [];
+        var pid = ACPToolKit.getCurrentParticipantId();
+        formResponses.unshift({ name: 'pid', value: pid });
+        formResponses.forEach(function (item) {
+            headers.push(item.name);
+            data.push(item.value);
+        });
+        arrayToCSV([headers, data], 'acp-' + pid + '-' + type);
+    }
+
+    function arrayToCSV (twoDiArray, fileName) {
+        //  http://stackoverflow.com/questions/17836273/export-javascript-data
+        //  -to-csv-file-without-server-interaction
+        var csvRows = [];
+        for (var i = 0; i < twoDiArray.length; ++i) {
+            for (var j = 0; j < twoDiArray[i].length; ++j) {
+                twoDiArray[i][j] = '\"' + twoDiArray[i][j] + '\"';
+            }
+            csvRows.push(twoDiArray[i].join(','));
+        }
+
+        var csvString = csvRows.join('\r\n');
+        var $a = $('<a></a>', {
+                href: 'data:attachment/csv;charset=utf-8,' + escape(csvString),
+                target: '_blank',
+                download: fileName + '.csv'
+            });
+
+        $('body').append($a[0]);
+        $a.get(0).click();
+        $a.remove();  
+    }
 
     $(function () {
         // Populate interface with current participant's ID
