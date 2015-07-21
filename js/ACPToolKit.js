7b57f58f5d0f238fe66356cd8654cd2c7110351a
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-07-22 04:04:33 +0800

    Complete skeleton code

--- js/ACPToolKit.js
+++ js/ACPToolKit.js
@@ -13,7 +13,6 @@ var ACPToolKit = (function () {
             pid = prompt('Enter current participant ID:').toString();
             this.setCurrentParticipantId(pid);
         }
-        console.log(pid)
         return pid;
     }
 
@@ -23,7 +22,7 @@ var ACPToolKit = (function () {
         });        
     }
 
-    module.generateFormInCSV = function (formResponses, type) {
+    module.downloadFormData = function (formResponses, type) {
         var headers = [];
         var data = [];
         var pid = ACPToolKit.getCurrentParticipantId();
@@ -33,6 +32,11 @@ var ACPToolKit = (function () {
             data.push(item.value);
         });
         arrayToCSV([headers, data], 'acp-' + pid + '-' + type);
+    }
+
+    module.downloadTrialResults = function (data) {
+        var pid = ACPToolKit.getCurrentParticipantId();
+        arrayToCSV(data, 'acp-' + pid + '-trials');
     }
 
     function arrayToCSV (twoDiArray, fileName) {
@@ -66,57 +70,76 @@ var ACPToolKit = (function () {
         }
     });
 
-    var wm = new WindowManager('autocompaste-display');
+    if (window.location.pathname.indexOf('experiment') > -1) {
+        var wm = new WindowManager('autocompaste-display');
+        var currentTrialOptions = null;
+        var startTime = null;
 
-    module.showWindows = function (options) {
+        module.presentTrial = function (options) {
+            startTime = new Date().getTime();
+            currentTrialOptions = options;
 
-        var data_file = options.data_file;
-        var stimuli = options.stimuli;
+            var data_file = options.data_file;
+            var stimuli = options.stimuli;
 
-        $('.js-expt-technique').text(options.technique);
-        $('.js-expt-granularity').text(options.granularity);
-        $('.js-expt-stimuli').text(options.stimuli);
-        
-        // Clean up DOM
-        wm.destroyAllWindows();
-        $('#autocompaste-completion').remove();
-        $('#autocompaste-measure-num-wrapped-lines').remove();
-        $('#autocompaste-measure-get-single-line-height').remove();
-        $('#autocompaste-measure-text-length-in-pixels').remove();
-        $('#autocompaste-completion').remove();
+            $('.js-expt-technique').text(options.technique);
+            $('.js-expt-granularity').text(options.granularity);
+            $('.js-expt-stimuli').text(options.stimuli);
+            
+            // Clean up DOM
+            wm.destroyAllWindows();
+            $('#autocompaste-completion').remove();
+            $('#autocompaste-measure-num-wrapped-lines').remove();
+            $('#autocompaste-measure-get-single-line-height').remove();
+            $('#autocompaste-measure-text-length-in-pixels').remove();
+            $('#autocompaste-completion').remove();
 
-        switch (options.technique) {
-            case 'TRADITIONAL':
-                var engine = null;
-                break;
-            case 'ACP':
-            default:
-                var engine = new AutoComPaste.Engine();        
-                break;
-        }
-        
-        var iface = new AutoComPaste.Interface(wm, engine, data_file);
-
-        // Highlight the relevant text.
-        iface.addEventListener('loaded', function () {
-            var lines_to_highlight = stimuli.split("\n\n");
-        
-            var windows = wm.getWindowList();
-            for (var i = 0; i < windows.length; i++) {
-                if (windows[i] == 'text_editor') {
-                    continue;
-                }
-
-                var win = wm.getWindowContent(windows[i]);
-                var content = $(win).find('pre').html();
-                lines_to_highlight.map (function (value, index, array) {
-                    content = content.replace (value,
-                    "<span class=\"highlighted\">" + value + "</span>");
-                });
-
-              $(win).find('pre').empty().append(content);
+            switch (options.technique) {
+                case 'TRADITIONAL':
+                    var engine = null;
+                    break;
+                case 'ACP':
+                default:
+                    var engine = new AutoComPaste.Engine();        
+                    break;
             }
-        });
+            
+            var iface = new AutoComPaste.Interface(wm, engine, data_file);
+
+            // Highlight the relevant text.
+            iface.addEventListener('loaded', function () {
+                var lines_to_highlight = stimuli.split("\n\n");
+            
+                var windows = wm.getWindowList();
+                for (var i = 0; i < windows.length; i++) {
+                    if (windows[i] == 'text_editor') {
+                        continue;
+                    }
+
+                    var win = wm.getWindowContent(windows[i]);
+                    var content = $(win).find('pre').html();
+                    lines_to_highlight.map (function (value, index, array) {
+                        content = content.replace (value,
+                        "<span class=\"highlighted\">" + value + "</span>");
+                    });
+
+                  $(win).find('pre').empty().append(content);
+                }
+            });
+        }
+
+        module.getCurrentTrialState = function () {
+            if (!currentTrialOptions) {
+                console.error('There is no trial running right now!');
+                return {};
+            }
+            var endTime = new Date().getTime();
+            currentTrialOptions.start_time = startTime;
+            currentTrialOptions.end_time = endTime;
+            currentTrialOptions.duration = endTime - startTime;
+            currentTrialOptions.user_response = $.trim($('.autocompaste-textarea').val());
+            return currentTrialOptions;
+        }
     }
 
     return module;
